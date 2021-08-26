0ce0d39888cd6d68413c878b0228159851b9a9db
Graham Hill <gthill97@gmail.com>
2021-08-26 13:30:38 -0600

    Got Lexurgy plugged into the app

--- components/editor.js
+++ components/editor.js
@@ -2,12 +2,21 @@ import React from "react";
 import styles from '../styles/Home.module.css'
 
 export default class Editor extends React.Component {
+  static defaultProps = {
+    editable: true,
+  };
+
   render() {
     const containerStyle = styles[this.props.id + "Container"];
     return (
       <div className={styles.editor + ' ' + containerStyle}>
         <label htmlFor={this.props.id}>{this.props.label}</label>
-        <textarea id={this.props.id}/>
+        <textarea
+          id={this.props.id}
+          value={this.props.value}
+          onChange={this.props.handleChange}
+          readOnly={!this.props.editable}
+        />
       </div>
     )
   }
--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,119 @@
+import React from "react";
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Editor from "../components/editor"
 import Arrow from "../components/arrow"
+import kotlin from "kotlin";
+import lexurgy from "../lib/lexurgy";
 
 const version = "0.15.1"
 const releaseUrl = "https://github.com/def-gthill/lexurgy/releases/tag/v" + version
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Lexurgy SC</title>
-        <meta name="description" content="A high-powered sound change applier"/>
-        <link rel="icon" href="/favicon.ico"/>
-      </Head>
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      changes: "",
+      output: "",
+    }
+    this.updateInput = this.updateInput.bind(this)
+    this.updateChanges = this.updateChanges.bind(this)
+    this.runLexurgy = this.runLexurgy.bind(this)
+  }
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Lexurgy Sound Changer
-        </h1>
+  render() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Lexurgy SC</title>
+          <meta name="description" content="A high-powered sound change applier"/>
+          <link rel="icon" href="/favicon.ico"/>
+        </Head>
 
-        <div className={styles.grid}>
-          <Editor id="input" label="Input Words"/>
-          <Arrow/>
-          <Editor id="changes" label="Sound Changes"/>
-          <Arrow/>
-          <Editor id="output" label="Output"/>
-        </div>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Lexurgy Sound Changer
+          </h1>
 
-        {/*<div className={styles.grid}>*/}
-        {/*  <a href="https://nextjs.org/docs" className={styles.card}>*/}
-        {/*    <h2>Documentation &rarr;</h2>*/}
-        {/*    <p>Find in-depth information about Next.js features and API.</p>*/}
-        {/*  </a>*/}
+          <div className={styles.grid}>
+            <Editor
+              id="input"
+              label="Input Words"
+              value={this.state.input}
+              handleChange={this.updateInput}
+            />
+            <Arrow/>
+            <Editor
+              id="changes"
+              label="Sound Changes"
+              value={this.state.changes}
+              handleChange={this.updateChanges}
+            />
+            <Arrow/>
+            <Editor
+              id="output"
+              label="Output"
+              value={this.state.output}
+              editable={false}
+            />
+          </div>
 
-        {/*  <a href="https://nextjs.org/learn" className={styles.card}>*/}
-        {/*    <h2>Learn &rarr;</h2>*/}
-        {/*    <p>Learn about Next.js in an interactive course with quizzes!</p>*/}
-        {/*  </a>*/}
+          <button className={styles.apply} onClick={this.runLexurgy}>Apply</button>
 
-        {/*  <a*/}
-        {/*    href="https://github.com/vercel/next.js/tree/master/examples"*/}
-        {/*    className={styles.card}*/}
-        {/*  >*/}
-        {/*    <h2>Examples &rarr;</h2>*/}
-        {/*    <p>Discover and deploy boilerplate example Next.js projects.</p>*/}
-        {/*  </a>*/}
+          {/*<div className={styles.grid}>*/}
+          {/*  <a href="https://nextjs.org/docs" className={styles.card}>*/}
+          {/*    <h2>Documentation &rarr;</h2>*/}
+          {/*    <p>Find in-depth information about Next.js features and API.</p>*/}
+          {/*  </a>*/}
 
-        {/*  <a*/}
-        {/*    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
-        {/*    className={styles.card}*/}
-        {/*  >*/}
-        {/*    <h2>Deploy &rarr;</h2>*/}
-        {/*    <p>*/}
-        {/*      Instantly deploy your Next.js site to a public URL with Vercel.*/}
-        {/*    </p>*/}
-        {/*  </a>*/}
-        {/*</div>*/}
-      </main>
+          {/*  <a href="https://nextjs.org/learn" className={styles.card}>*/}
+          {/*    <h2>Learn &rarr;</h2>*/}
+          {/*    <p>Learn about Next.js in an interactive course with quizzes!</p>*/}
+          {/*  </a>*/}
 
-      <footer className={styles.footer}>
-        Powered by <a href={releaseUrl}>Lexurgy {version}</a>
-      </footer>
-    </div>
-  )
+          {/*  <a*/}
+          {/*    href="https://github.com/vercel/next.js/tree/master/examples"*/}
+          {/*    className={styles.card}*/}
+          {/*  >*/}
+          {/*    <h2>Examples &rarr;</h2>*/}
+          {/*    <p>Discover and deploy boilerplate example Next.js projects.</p>*/}
+          {/*  </a>*/}
+
+          {/*  <a*/}
+          {/*    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
+          {/*    className={styles.card}*/}
+          {/*  >*/}
+          {/*    <h2>Deploy &rarr;</h2>*/}
+          {/*    <p>*/}
+          {/*      Instantly deploy your Next.js site to a public URL with Vercel.*/}
+          {/*    </p>*/}
+          {/*  </a>*/}
+          {/*</div>*/}
+        </main>
+
+        <footer className={styles.footer}>
+          Powered by <a href={releaseUrl}>Lexurgy {version}</a>
+        </footer>
+      </div>
+    )
+  }
+
+  updateInput(event) {
+    const newInput = event.target.value
+    this.setState({input: newInput})
+  }
+
+  updateChanges(event) {
+    const newChanges = event.target.value
+    this.setState({changes: newChanges})
+  }
+
+  runLexurgy() {
+    const sc = lexurgy.com.meamoria.lexurgy.sc
+    const input = new kotlin.kotlin.collections.ArrayList(this.state.input.split("\n"))
+    const soundChanger = sc.SoundChanger.Companion.fromLsc_61zpoe$(this.state.changes)
+    const output = soundChanger.change_i6pp2q$(input).toArray()
+    this.setState({output: output.join("\n")})
+  }
 }
