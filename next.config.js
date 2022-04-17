55d4f50d94eeaa9900d24b02dfcdc51335e35221
Graham Hill <gthill97@gmail.com>
2022-04-17 11:03:32 -0600

    Remove example files

--- next.config.js
+++ next.config.js
@@ -4,24 +4,6 @@ const CopyPlugin = require("copy-webpack-plugin")
 module.exports = {
   reactStrictMode: true,
   target: "serverless",
-  // Garbage solution to obvious flaw in NextJS,
-  // https://github.com/vercel/next.js/issues/8251#issuecomment-854148718
-  webpack: function (config, { dev, isServer }) {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback.fs = false
-    }
-    // copy files you're interested in
-    if (!dev) {
-      config.plugins.push(
-        new CopyPlugin({
-          patterns: [{ from: "files", to: "files" }],
-        })
-      )
-    }
-
-    return config
-  },
   redirects: async function() {
     return [
       {
